8af52f3efd39d6fee693b53d0d773258e0c078eb
ZahraFa <faratusa2@gmail.com>
2024-11-20 11:24:41 -0800

    add articles & add profile

--- article/models.py
+++ article/models.py
@@ -3,8 +3,18 @@ from datetime import timezone
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+
+
 class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    category = models.ManyToManyField(Category)
     title = models.CharField(max_length=70, blank=True, null=True)
     body = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to="images/articles", blank=True, null=True)
