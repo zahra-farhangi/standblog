091ea4f8473597777938bbf242cdcf167b597731
ZahraFa <faratusa2@gmail.com>
2024-12-12 08:32:30 +0330

    add login form and add register form

---
+++ account/forms.py
@@ -0,0 +1,64 @@
+from django import forms
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.forms import ValidationError
+import re
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'input100'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input100'}))
+
+
+    def clean_password(self):
+        user = authenticate(username=self.cleaned_data.get('username'), password=self.cleaned_data.get('password'))
+        if user is not None:
+            return self.cleaned_data.get('password')
+        raise ValidationError('Incorrect username or password.', code='invalid_info')
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'input100'}), label='Username')
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'input100'}), label='Email')
+    password1 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class': 'input100'}), label='Password')
+    password2 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class': 'input100'}), label='Confirm password')
+
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+
+        # Check Username len
+        if len(username) < 5:
+            raise ValidationError('Username must be at least 5 characters.' , code='Invalid_Username')
+
+        # Check Username Unique
+        if User.objects.filter(username=username).exists():
+            raise ValidationError('Username already exists.', code='Username_Exists')
+
+        return username
+
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('Email already exists.', code='Email_Exists')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+
+        # Check Match Password
+        if password1 and password2 and password1 != password2:
+            self.add_error('password2',r"Passwords don't match.")
+
+        # Check Characters Password
+        if password1:
+            if len(password1) < 8:
+                self.add_error('password1','Password must be at least 8 characters.')
+            if not re.search(r'[A-Z]', password1):
+                self.add_error('password1','Password must contain at least one uppercase')
+            if not re.search(r'[a-z]', password1):
+                self.add_error('password1','Password must contain at least one lowercase')
+            if not re.search(r'[0-9]', password1):
+                self.add_error('password1','Password must contain at least one number')
+        return cleaned_data
--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
+from .forms import LoginForm, RegisterForm
+
 
 #  login
 def login_view(request):
@@ -9,35 +11,61 @@ def login_view(request):
         return redirect('home:main')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = get_object_or_404(User, username=form.cleaned_data.get('username'))
             login(request, user)
-            return redirect('home:main')
-        else:
-            messages.warning(request, 'Invalid username or password')
-            return redirect("account:login")
-    return render(request, 'account/login.html')
+            return redirect("home:main")
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+    # username = request.POST.get('username')
+    # password = request.POST.get('password')
+    # user = authenticate(request, username=username, password=password)
+    # if user is not None:
+    #     login(request, user)
+    #     return redirect('home:main')
+    # else:
+    #     messages.warning(request, 'Invalid username or password')
+    #     return redirect("account:login")
 
 
 def register_view(request):
-    context = {"errors": []}
+    # context = {"errors": []}
     if request.user.is_authenticated:
         return redirect('home:main')
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password1 = request.POST.get('password1')
-        password2 = request.POST.get('password2')
-        if password1 != password2:
-            context['errors'].append("passwords don't match")
-            return render(request, 'account/register.html', context)
 
-        user =User.objects.create(username=username, email=email, password=password1)
-        login(request, user)
-        return redirect('home:main')
-    return render(request, 'account/register.html', {})
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password1')
+
+            user = User.objects.create_user(username=username, email=email, password=password)
+            login(request, user)
+            messages.success(request, 'Account was created successfully!')
+            return redirect('home:main')
+
+        return render(request, 'account/register.html', {'form': form})
+    else:
+        form = RegisterForm()
+        return render(request, 'account/register.html', {'form': form})
+
+
+#     username = request.POST.get('username')
+#     email = request.POST.get('email')
+#     password1 = request.POST.get('password1')
+#     password2 = request.POST.get('password2')
+#     if password1 != password2:
+#         context['errors'].append("passwords don't match")
+#         return render(request, 'account/register.html', context)
+#
+#     user =User.objects.create(username=username, email=email, password=password1)
+#     login(request, user)
+#     return redirect('home:main')
+# return render(request, 'account/register.html', {})
 
 
 def logout_view(request):
